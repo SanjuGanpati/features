f859afdb87c041b3ffb5e5d7c67499b4b46aac6c
Sangeeta <sangeetaganpati1@gmail.com>
2025-01-09 00:50:44 +1100

    Update TestCase_withHooksPlugin.java

--- untitled2/src/test/java/api/cucumber/Runner/TestCase_withHooksPlugin.java
+++ untitled2/src/test/java/api/cucumber/Runner/TestCase_withHooksPlugin.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptionss(features = {"src/test/java/api/cucumber/features/TestDemo.feature"},
+@CucumberOptions(features = {"src/test/java/api/cucumber/features/TestDemo.feature"},
         glue = {"api.cucumber.steps", "api.cucumber.hooks"},
        monochrome = true,
         plugin = {"pretty", "html:target/HtmlReports"} , dryRun = false)
