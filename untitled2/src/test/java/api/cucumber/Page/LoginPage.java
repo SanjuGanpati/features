e35a543c8e2263e7f71b521863ce84384a35af84
Sangeeta <sangeetaganpati1@gmail.com>
2025-01-09 23:39:36 +1100

    Update LoginPage.java

--- untitled2/src/test/java/api/cucumber/Page/LoginPage.java
+++ untitled2/src/test/java/api/cucumber/Page/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LoginPageee {
+public class LoginPage {
     private WebDriver driver;
     private By txt_username = By.id("username");
     private By txt_password = By.id("password");
